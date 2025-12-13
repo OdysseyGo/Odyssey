21a0cd06b0ece6c69d230701a6fb3ec6ad0f3edb
Bora Akoğuz <66472581+boraakoguz@users.noreply.github.com>
2025-12-14 01:52:57 +0300

    Clean up imports in _layout.tsx
    Removed unused import statements for Colors and useColorScheme.

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -6,9 +6,6 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { ThemeName } from '@/constants/Colors';
-import Colors from '@/constants/Colors';
-
-import { useColorScheme } from '@/components/useColorScheme';
 import { useColorTheme } from '@/utils/useColorTheme';
 import Colors from '@/constants/Colors';
 
