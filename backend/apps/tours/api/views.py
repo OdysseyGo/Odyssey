051934fe8b98da727114cf48e26a85a15e235bc6
Can Kutukoglu <cankutukoglu03@gmail.com>
2025-12-22 23:51:42 +0300

    KAN-45 Fixed import

--- backend/apps/tours/api/views.py
+++ backend/apps/tours/api/views.py
@@ -1,7 +1,6 @@
 from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, permissions, viewsets
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
